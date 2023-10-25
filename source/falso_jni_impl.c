fbdf955de695bae7a518093a356823f2c481201e
Volodymyr Atamanenko <v@atamanenko.in>
2023-10-25 15:16:54 +0100

    v0.5 - Fixes & improvements from BBB (#3)
    * Added 15 new symbols to `dynlib.c`
    * Added 2 commonly used constants to `falso_jni_impl.c`
    * Improved `__android_log_*` formatting
    * Rewrote `eglChooseConfig` to act according to the specs

--- source/falso_jni_impl.c
+++ source/falso_jni_impl.c
@@ -1,5 +1,9 @@
 #include <falso_jni/FalsoJNI_Impl.h>
 
+/*
+ * JNI Methods
+*/
+
 NameToMethodID nameToMethodId[] = {};
 
 MethodsBoolean methodsBoolean[] = {};
@@ -13,15 +17,35 @@ MethodsObject methodsObject[] = {};
 MethodsShort methodsShort[] = {};
 MethodsVoid methodsVoid[] = {};
 
-NameToFieldID nameToFieldId[] = {};
+/*
+ * JNI Fields
+*/
+
+// System-wide constant that applications sometimes request
+// https://developer.android.com/reference/android/content/Context.html#WINDOW_SERVICE
+char WINDOW_SERVICE[] = "window";
+
+// System-wide constant that's often used to determine Android version
+// https://developer.android.com/reference/android/os/Build.VERSION.html#SDK_INT
+// Possible values: https://developer.android.com/reference/android/os/Build.VERSION_CODES
+const int SDK_INT = 19; // Android 4.4 / KitKat
+
+NameToFieldID nameToFieldId[] = {
+		{ 0, "WINDOW_SERVICE", FIELD_TYPE_OBJECT }, 
+		{ 1, "SDK_INT", FIELD_TYPE_INT },
+};
 
 FieldsBoolean fieldsBoolean[] = {};
 FieldsByte fieldsByte[] = {};
 FieldsChar fieldsChar[] = {};
 FieldsDouble fieldsDouble[] = {};
 FieldsFloat fieldsFloat[] = {};
-FieldsInt fieldsInt[] = {};
-FieldsObject fieldsObject[] = {};
+FieldsInt fieldsInt[] = {
+		{ 1, SDK_INT },
+};
+FieldsObject fieldsObject[] = {
+		{ 0, WINDOW_SERVICE },
+};
 FieldsLong fieldsLong[] = {};
 FieldsShort fieldsShort[] = {};
 
